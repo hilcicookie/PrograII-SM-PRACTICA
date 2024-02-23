13874a0f107551804f94931add2e320db28ea45e
hilcicookie <hilciacontreras777@gmail.com>
2024-02-23 08:46:56 -0600

    actualizacionSensoresacelerometro

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -12,71 +12,58 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.graphics.Color;
-
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
     SensorManager sensorManager;
     Sensor sensor;
     SensorEventListener sensorEventListener;
+    TextView tempVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorLuz);
-        activarSensorLuz();
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
+        acvtivarSensorAcelerometro();
     }
-
     @Override
     protected void onResume() {
-        iniciar();
+        inicar();
         super.onResume();
     }
-
     @Override
     protected void onPause() {
         detener();
         super.onPause();
     }
-
-    private void activarSensorLuz() {
+    private void acvtivarSensorAcelerometro(){
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if (sensor == null) {
-            tempVal.setText("Tu telefono NO tiene sensor de Luz");
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null){
+            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                double valor = sensorEvent.values[0];
-                tempVal.setText("Luz: " + valor);
-                if (valor <= 20) {
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                } else if (valor <= 50) {
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                } else {
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
+                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0]+ "; Y="+ sensorEvent.values[1] +"; Z="+
+                        sensorEvent.values[2]);
             }
-
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
 
             }
         };
     }
-
-    private void iniciar() {
-        sensorManager.registerListener(sensorEventListener, sensor, 2000 * 1000);
+    private void inicar(){
+        sensorManager.registerListener(sensorEventListener, sensor,2000*1000 );
     }
-
-    private void detener() {
+    private void detener(){
         sensorManager.unregisterListener(sensorEventListener);
     }
 }
