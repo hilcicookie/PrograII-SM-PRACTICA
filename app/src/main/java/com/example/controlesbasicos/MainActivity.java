f707854d410d8273ca2cd6f3cbd7d2ff7d220823
hilcicookie <hilciacontreras777@gmail.com>
2024-02-23 10:45:23 -0600

    actualizacion parcial

--- app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -1,69 +1,74 @@
 package com.example.controlesbasicos;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.widget.Toast;
-import android.util.Log;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-public class MainActivity extends AppCompatActivity {
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
-    TextView tempVal;
+import androidx.appcompat.app.AppCompatActivity;
 
+
+public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
+    TextView tempVal;
+    Spinner spn;
+    Button btn;
+    Area area = new Area();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        acvtivarSensorAcelerometro();
-    }
-    @Override
-    protected void onResume() {
-        inicar();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    private void acvtivarSensorAcelerometro(){
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(sensor==null){
-            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0]+ "; Y="+ sensorEvent.values[1] +"; Z="+
-                        sensorEvent.values[2]);
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
 
+        tbh = findViewById(R.id.tbhParcial1);
+        tbh.setup();
+        tbh.addTab(tbh.newTabSpec("AREA").setIndicator("AREAA", null).setContent(R.id.Area));
+        tbh.addTab(tbh.newTabSpec("TARI").setIndicator("TARIFA",null).setContent(R.id.Tarifa));
+
+        btn = findViewById(R.id.btnConvertir1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spn = findViewById(R.id.spnDeArea);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAArea);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidad1);
+                try {
+                    double cantidad = Double.parseDouble(tempVal.getText().toString());
+                    double resp = area.convertir(0, de, a, cantidad);
+                    mostrarResultado(resp);
+                } catch (NumberFormatException e) {
+                    mostrarError("Ingresa una cantidad válida.");
+                }
             }
-        };
+        });
+
     }
-    private void inicar(){
-        sensorManager.registerListener(sensorEventListener, sensor,2000*1000 );
+
+    private void mostrarResultado(double resultado) {
+        String mensaje = String.format("Respuesta: %.1f", resultado);
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
     }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
+
+    private void mostrarError(String mensaje) {
+        Toast.makeText(getApplicationContext(), "Error: " + mensaje, Toast.LENGTH_LONG).show();
+    }
+}
+
+class Area {
+    double[][] valores =  {
+            {1,6988, 75218.1332, 10000,  0.0001187, 16, 0.6988}
+    };
+
+    public double convertir(int opcion, int de, int a, double cantidad) {
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
